c3280b2fb7b05b98b9063bf0a987bc877dc2faa7
sobrabd <42965564+sobrabd@users.noreply.github.com>
2019-03-08 11:06:27 +0300

    Tl 23 (#17)
    * TL-23. Settings edited, requirements edited, README & Makefile added

--- file_service/settings.py
+++ file_service/settings.py
@@ -1,5 +1,5 @@
 import os
-from configurations import Configuration, values
+from configurations import Configuration
 import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,9 +22,7 @@ class BaseConfiguration(Configuration):
     # DOTENV = os.path.join(BASE_DIR, '.env')
     
     # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = values.Value(
-        '783ae16754d4ce6d1de0a749fb0744f4', environ_prefix=''
-    )
+    SECRET_KEY = '783ae16754d4ce6d1de0a749fb0744f4'
     
     # FIXME: we must setup that list
     ALLOWED_HOSTS = ['*', ]
@@ -57,11 +55,12 @@ class BaseConfiguration(Configuration):
 
     ROOT_URLCONF = 'file_service.urls'
 
-    TROOD_AUTH_SERVICE_URL = values.URLValue(
-        'http://authorization.trood:8000/', environ_prefix='')
 
-    SERVICE_DOMAIN = values.Value('', environ_prefix='')
-    SERVICE_AUTH_SECRET = values.Value('', environ_prefix='')
+    TROOD_AUTH_SERVICE_URL = os.environ.get(
+        'TROOD_AUTH_SERVICE_URL', 'http://authorization.trood:8000/'
+        )
+    SERVICE_DOMAIN = os.environ.get("SERVICE_DOMAIN", "FILESERVICE")
+    SERVICE_AUTH_SECRET =  os.environ.get("SERVICE_AUTH_SECRET")
 
     REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -96,7 +95,7 @@ class BaseConfiguration(Configuration):
         ('en-US', 'English'),
     )
 
-    LANGUAGE_CODE = values.Value('en-US', environ_prefix='')
+    LANGUAGE_CODE =  os.environ.get('LANGUAGE_CODE', 'en-US')
 
     TIME_ZONE = 'UTC'
 
@@ -109,13 +108,13 @@ class BaseConfiguration(Configuration):
     DATE_FORMAT = '%d-%m-%Y'
 
     MEDIA_URL = '/media/'
-    MEDIA_ROOT = values.Value(rel('media'), environ_prefix='')
+    MEDIA_ROOT = os.environ.get('FILE_SERVICE_MEDIA_ROOT', rel('media'))
 
     STATIC_URL = '/static/'
-    STATIC_ROOT = values.Value(rel('static'), environ_prefix='')
+    STATIC_ROOT =  os.environ.get('FILE_SERVICE_STATIC_ROOT', rel('static'))
 
     # Absolute url
-    FILES_BASE_URL = values.Value('/media/', environ_prefix='')
+    FILES_BASE_URL = os.environ.get('FILES_BASE_URL', '/media/')
 
 
     IMAGE_SIZES = {
