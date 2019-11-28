4726b3d6def794c14da390ca83edbbacb388afb3
sobrabd <42965564+sobrabd@users.noreply.github.com>
2019-11-28 16:01:54 +0300

    TL-829. textextration error handling added. textextraction improve (#28)

--- plugins/text_extractor.py
+++ plugins/text_extractor.py
@@ -24,7 +24,7 @@ class TextExtractorPlugin(TroodBasePlugin):
             "application/vnd.ms-word.document.macroEnabled.12",
             "application/vnd.ms-word.template.macroEnabled.12", "application/vnd.ms-excel",
             "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-            "application/vnd.openxmlformats-officedocument.spreadsheetml.template", 
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.template",
             "application/vnd.ms-excel.sheet.macroEnabled.12",
             "application/vnd.ms-excel.template.macroEnabled.12",
             "application/vnd.ms-excel.addin.macroEnabled.12",
@@ -33,14 +33,14 @@ class TextExtractorPlugin(TroodBasePlugin):
             "application/vnd.openxmlformats-officedocument.presentationml.presentation",
             "application/vnd.openxmlformats-officedocument.presentationml.template",
             "application/vnd.openxmlformats-officedocument.presentationml.slideshow",
-            "application/vnd.ms-powerpoint.addin.macroEnabled.12", 
+            "application/vnd.ms-powerpoint.addin.macroEnabled.12",
             "application/vnd.ms-powerpoint.presentation.macroEnabled.12",
             "application/vnd.ms-powerpoint.template.macroEnabled.12",
-            "application/vnd.ms-powerpoint.slideshow.macroEnabled.12", 
-            "application/vnd.oasis.opendocument.spreadsheet", 
+            "application/vnd.ms-powerpoint.slideshow.macroEnabled.12",
+            "application/vnd.oasis.opendocument.spreadsheet",
             "application/vnd.oasis.opendocument.text",
             "application/vnd.oasis.opendocument.presentation"
-            ]
+        ]
     }
 
     @classmethod
@@ -51,11 +51,32 @@ class TextExtractorPlugin(TroodBasePlugin):
     def extract(cls, sender, **kwargs):
         file = kwargs.get('instance')
         config = cls.get_config()
-        if file.mimetype in config['extractable_mimetypes']:
-            filepath = file.file.path
-            b_text = textract.process(filepath)
-            title = file.origin_filename.split('.')[0]
-            raw_text = b_text.decode("utf-8")
-            text = re.sub(r'\s+', ' ', re.sub(r'<[^<]+>', ' ', raw_text))
-            FileTextContent.objects.create(source=file, content=text, title=title)
+
+        extractable_mimetype = file.mimetype in config['extractable_mimetypes']
+
+        not_extracted = True if not file.metadata or file.metadata.get(
+            'text_extracted', 'not_extracted') == 'not_extracted' else False
+
+        if extractable_mimetype and not_extracted:
+            try:
+                filepath = file.file.path
+                b_text = textract.process(filepath)
+                title = file.origin_filename.split('.')[0]
+                raw_text = b_text.decode("utf-8")
+                text = re.sub(r'\s+', ' ', re.sub(r'<[^<]+>', ' ', raw_text))
+                FileTextContent.objects.create(
+                    source=file, content=text, title=title)
+                cls._set_metadata(file, True)
+            except: 
+                cls._set_metadata(file, False)
+
+    @classmethod
+    def _set_metadata(self, file, value):
+        if not file.metadata:
+            metadata = {'text_extracted': value}
+            file.metadata = metadata
+            file.save()
+        else:
+            file.metadata['text_extracted'] = value
+            file.save()
 
